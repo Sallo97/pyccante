f480231debd3b43222bd201f9585e36381cacda2
Salvatore Salerno <salerno.salvatore1997@gmail.com>
2023-05-24 19:33:25 +0200

    added a Table

--- pyccanteGUI/main.py
+++ pyccanteGUI/main.py
@@ -1,15 +1,32 @@
 import sys
 import pyccante as pyc
 from PySide6.QtWidgets import (QApplication, QDialog, QPushButton,
-                               QLineEdit, QVBoxLayout, QTreeWidget, QTreeWidgetItem)
+                               QLineEdit, QVBoxLayout, QTableWidget, QTableWidgetItem)
 
 
 class Form(QDialog):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
+        self.categories = {"Filters": ["Luminance", "Bilateral2df", "Bilateral2ds", "Conv2D", "Max", "Mean", "Med",
+                                       "MedVec", "Mosaic", "Warp2D", "Rotation"],
+                           "Algorithms": ["HDRMerger", "GrowCut", "PoissonSolver", "ConnectedComponents"],
+                           "ToneMapping": ["Ward Histogram TMO", "Reinhard TMO", "Exposure Fusion", "Durand TMO",
+                                           "Drago TMO"]}
+
         self.img = pyc.Image()
         self.setWindowTitle("Pyccante GUI")
+
+        # Define tree
+        self.table = QTableWidget()
+        self.table.setRowCount(len(self.categories.get("Filters")))
+        self.table.setColumnCount(3)
+        self.table.setHorizontalHeaderLabels(["Filter", "Algorithms", "Tone Mapping"])
+        self.construct_table()
+
+        # Define box to write
         self.edit = QLineEdit("Image path here.")
+
+        # Define buttons
         self.read_button = QPushButton("Read Image")
         self.copy_button = QPushButton("Copy Image")
 
@@ -21,6 +38,14 @@ class Form(QDialog):
         layout.addWidget(self.edit)
         layout.addWidget(self.read_button)
         layout.addWidget(self.copy_button)
+        layout.addWidget(self.table)
+
+    def construct_table(self):
+        i = 0
+        for i, (key, values) in enumerate(self.categories.items()):
+            for j, value in enumerate(values):
+                item_name = QTableWidgetItem(value)
+                self.table.setItem(j, i, item_name)
 
     def read_img(self):
         print(f"Reading the image {self.edit.text()}...")
